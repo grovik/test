a98a4d29f2d7471354333343aaa9f77ce51b22a6
Josef (N) Němec <admin@josefnemec.cz>
2023-08-28 15:50:16 +0200

    objekt

--- index.php
+++ index.php
@@ -1,10 +1,39 @@
 <?php 
 
+class DataItem {
+    public int $id;
+    public string $name;
+    public int $firstNumber;
+    public int $secondNumber;
+    public int $thirdNumber;
+    public string $calculation;
+    public string $joke;
+    public string $createdAt;
+}
+
 // Načtení JSON dat ze zadané adresy
 $jsonData = file_get_contents('https://www.digilabs.cz/hiring/data.php');
 
-// Převedení JSON dat na asociativní pole
-$data = json_decode($jsonData, true);
 
+$dataArray = json_decode($jsonData);
 
+$processedData = [];
 
+foreach ($dataArray as $item) {
+    $dataItem = new DataItem();
+    $dataItem->id = $item->id;
+    $dataItem->name = $item->name;
+    $dataItem->firstNumber = $item->firstNumber;
+    $dataItem->secondNumber = $item->secondNumber;
+    $dataItem->thirdNumber = $item->thirdNumber;
+    $dataItem->calculation = $item->calculation;
+    $dataItem->joke = $item->joke;
+    $dataItem->createdAt = $item->createdAt;
+    
+    $processedData[] = $dataItem;
+}
+
+// Zde máte zpracovaná data ve formě objektů
+print_r($processedData);
+
+?>
